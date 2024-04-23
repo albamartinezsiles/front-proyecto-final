c4fe58d779f94fe638cd9d98f7484212c835a89c
Alba <dok-do@hotmail.es>
2024-04-23 10:56:14 +0200

    arreglito

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
       return; 
     }
   
-    fetch('https://api-proyecto-final.onrender.com/tareas')
+    fetch('/tareas')
       .then(respuesta => respuesta.json())
       .then(tareas => setTareas(tareas))
       .catch(error => console.error('Error al leer las tareas:', error));
@@ -34,7 +34,7 @@ function App() {
 
       <form onSubmit={(evento) => {
         evento.preventDefault();
-        fetch('https://api-proyecto-final.onrender.com/crear-tarea', {
+        fetch('/crear-tarea', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -84,7 +84,7 @@ function App() {
           onClick={async () => {
             if (editando && tarea === tareaEditada) {
               // Aquí va tu código para guardar los cambios
-              const respuesta = await fetch(`https://api-proyecto-final.onrender.com/tareas/editar/${tarea._id}`, {
+              const respuesta = await fetch(`/tareas/editar/${tarea._id}`, {
                 method: 'PUT',
                 headers: {
                   'Content-Type': 'application/json',
@@ -117,7 +117,7 @@ function App() {
         
         <button className="boton"
         onClick={() => {
-          fetch(`https://api-proyecto-final.onrender.com/tareas/borrar/${tarea._id}`, {
+          fetch(`/tareas/borrar/${tarea._id}`, {
             method: 'DELETE',
             headers: {
               'Content-Type': 'application/json',
@@ -139,7 +139,7 @@ function App() {
           onClick={() => {
             const nuevoEstado = tarea.estado === 'pendiente' ? 'terminada' : 'pendiente';
 
-            fetch(`https://api-proyecto-final.onrender.com/tareas/estado/${tarea._id}`, {
+            fetch(`/tareas/estado/${tarea._id}`, {
               method: 'PUT',
               headers: {
                 'Content-Type': 'application/json',
